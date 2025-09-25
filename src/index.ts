f22f2f4de20d2f9065cb2711a9426b35003ef91c
MacAndersonUche <macandersonuche@yahoo.com>
2025-09-25 01:13:54 +0100

    comment

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
+/**
+ *  WARNING: DO NOT RESTART THE SERVER !! 
+ * 
+ * This application uses in-memory storage (arrays) for data persistence.
+ * Restarting the server will LOSE ALL DATA.
+ * 
+ * Keep the server running to preserve your data!
+ */
+
 import express from "express";
 import routes from "./routes";
 
